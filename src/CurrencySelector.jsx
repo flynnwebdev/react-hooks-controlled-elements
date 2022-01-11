979878500022251b612ef29597dd9cfff8f06e3c
Flynn <flynnwebdev@gmail.com>
2022-01-11 14:11:03 +1000

    Mapped currencies.json to options

--- src/CurrencySelector.jsx
+++ src/CurrencySelector.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react"
+import { useState, useEffect } from "react";
+import currencies from "./currencies.json"
 
 export default function CurrencySelector({ currency, setCurrency }) {
-    return (
-        <>
-            <select value={currency} onChange={e => setCurrency(e.target.value)}>
-                <option value="AUD">Australian Dollar</option>
-                <option value="USD">US Dollar</option>
-                <option value="EUR">Euro</option>
-            </select>
-        </>
-    )
+  return (
+    <>
+      {currencies ? (
+        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+            {currencies.map(cur => <option value={cur.currency}>{cur.currency} ({cur.country})</option>)}
+        </select>
+      ) : (
+        <p>Loading ...</p>
+      )}
+    </>
+  );
 }
